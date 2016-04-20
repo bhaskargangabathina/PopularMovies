aeb1f9ce91749ca23e25e701695e0cb408fc4480
Bhaskar Gangabattina <bhaskar.gangabattina@verse.in>
2016-04-20 15:58:14 +0530

    second commit

--- app/src/main/java/bhaskarandroidnannodegree/popularmoviesstage2/fragments/DetailActivityFragment.java
+++ app/src/main/java/bhaskarandroidnannodegree/popularmoviesstage2/fragments/DetailActivityFragment.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 Anupam Das
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package bhaskarandroidnannodegree.popularmoviesstage2.fragments;
 
@@ -53,7 +38,7 @@ import bhaskarandroidnannodegree.popularmoviesstage2.utils.Utils;
 public class DetailActivityFragment extends Fragment implements LoaderCallbacks<Cursor> {
 
     private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
-    private static final String MOVIE_SHARE_HASHTAG = " #PopularMovieApp #ByAnupam ";
+    private static final String MOVIE_SHARE_HASHTAG = " #PopularMovieApp #ByBhaskar ";
     private static final int DETAIL_LOADER = 0;
     private static final int TRAILER_LOADER = 1;
     private static final int REVIEW_LOADER = 2;
